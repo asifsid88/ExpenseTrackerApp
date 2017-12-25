a374e10196e0bf03936eee94b316e3540a684b40
Ashique Hussain <mhussaa@amazon.com>
2017-12-25 18:52:12 +0530

    Integrated Redux

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-import { AppRegistry } from 'react-native';
+import 'babel-polyfill';
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import {createLogger} from 'redux-logger'
+import reduxMulti from 'redux-multi';
+import allReducers from './src/reducers';
 import App from './App';
 
-AppRegistry.registerComponent('ExpenseTrackerApp', () => App);
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+                allReducers,
+                applyMiddleware(
+                    thunkMiddleware,
+                    reduxMulti,
+                    loggerMiddleware
+                )
+              );
+
+const getApp = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+AppRegistry.registerComponent('ExpenseTrackerApp', () => getApp);
